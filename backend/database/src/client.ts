bdef34b9563f274aea8da646c23a03c146f13830
ParasharDeb <parashardeb11@gmail.com>
2026-09-12 12:57:31 +0530

    prisma setup

---
+++ backend/database/src/client.ts
@@ -0,0 +1,2 @@
+export { connectDatabase, db } from "./prisma/db.ts";
+export type { Contract } from "./prisma/contract.d.ts";
--- backend/video-transcode-worker/index.ts
+++ backend/video-transcode-worker/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { connectDatabase, db } from "@repo/database";
+
+await connectDatabase(); 
+async function dbcall(){
+    return db.orm.public.User.where({id:1}).select("email").first()
+}
+const users =await dbcall()
+console.log(users)
